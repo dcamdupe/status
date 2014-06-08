b14ed7e28bd5f694d5b27da828539138d2e221e9
dcamdupe <david@uberconcept.com>
2014-06-08 19:37:47 +1000

    Added working method to session controller

---
+++ site/Site/Controllers/Api/ApiSessionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Site.Services;
+
+namespace Site.Controllers
+{
+    public class SessionController : ApiController
+    {
+        private IApiServices _apiServices;
+
+        public SessionController(IApiServices apiServices)
+        {
+            _apiServices = apiServices;
+        }
+
+        public string Post(string login, string password)
+        {
+            return _apiServices.CreateSession(login, password);
+        }
+    }
+}
